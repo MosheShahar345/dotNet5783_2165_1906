0696dbdcede3124c0452713f8430278b39a1abab
moshe shahar <moshahar@g.jct.ac.il>
2022-10-27 19:50:12 +0300

    Initial code + Welcome functions

---
+++ Stage0/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Stage0;
+partial class Program
+{
+    static void Main(string[] args)
+    {
+        Welcome2165();
+        Welcome1906();
+        Console.ReadKey();
+    }
+
+    static partial void Welcome1906();
+    private static void Welcome2165()
+    {
+        Console.Write("Enter your name: ");
+        string userName = Console.ReadLine();
+        Console.WriteLine("{0}, welcome to my first console application", userName);
+    }
+}
