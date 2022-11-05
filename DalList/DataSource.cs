fb7d63600911747d3745ee6211d615f2fe9c021f
moshe shahar <moshahar@g.jct.ac.il>
2022-11-05 21:12:45 +0200

    new filse

---
+++ DalList/DataSource.cs
@@ -0,0 +1,25 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+using DO;
+
+namespace Dal;
+
+internal struct DataSource
+{
+    public static readonly int random = 0;
+    internal static Product[] product = new Product[50];
+    internal static Order[] order = new Order[100];
+    internal static OrderItem[] orderItem = new OrderItem[200];
+
+    static readonly int sizeOfProduct = 0;
+    static readonly int sizeOfOrder = 0;
+    static readonly int sizeOfOrderItem = 0;
+
+    private static void AddProduct(Product[] arr, int size);
+    private static void AddOrder(Order[] arr, int sizr);
+    private static void AddOrderItem(OrderItem[] arr, int size);
+}
---
+++ DalTest/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Dal
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            return 0;
+        }
+    }
+}
