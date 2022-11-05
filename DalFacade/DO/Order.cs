63fc379ff06f84ffcef3fad383de23640bb3c94b
moshe shahar <moshahar@g.jct.ac.il>
2022-11-06 00:01:14 +0200

    get/set

--- DalFacade/DO/Order.cs
+++ DalFacade/DO/Order.cs
@@ -8,7 +8,7 @@ namespace DO;
 
 public struct Order
 {
-    public int ID;
+    public int ID { get; set; }
     public string CustomerName { get; set; }
     public string CustomerEmail { get; set; }
     public string CustomerAdress { get; set; }
