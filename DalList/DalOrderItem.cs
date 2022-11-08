e8431c1ebeb9b0d82c948e8fc5ca72941447e3b1
Aharon Katz <ahkatz@g.jct.ac.il>
2022-11-08 13:08:37 +0200

    updating the dal order

--- DalList/DalOrderItem.cs
+++ DalList/DalOrderItem.cs
@@ -4,8 +4,20 @@
 //using System.Text;
 //using System.Threading.Tasks;
 
+using DO;
+using static Dal.DataSource;
+
 namespace Dal;
 
 public class DalOrderItem
 {
+   private static void AddOrderItem(OrderItem orderItem)
+    {
+        foreach (var item in DataSource.OrderItems)
+        {
+            if (item.OrderID == orderItem.OrderID && item.ProductID == orderItem.ProductID)
+                throw new ArgumentException("product already exist");
+        }
+        DataSource.OrderItems[Config.SizeOfOrderItem++] = orderItem;
+    }
 }
