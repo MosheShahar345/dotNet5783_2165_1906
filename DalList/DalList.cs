ecaca214a8ac523c12c4ec9e3a32a7e200080d7b
Aharon Katz <ahkatz@g.jct.ac.il>
2022-11-27 15:58:11 +0200

    changes

--- DalList/DalList.cs
+++ DalList/DalList.cs
@@ -4,7 +4,7 @@ using System.Security.Principal;
 
 namespace Dal;
 
-sealed public class DalList : IDal
+public sealed class DalList : IDal
 {
     public IProduct Product => new DalProduct();
     public IOrderItem OrderItem => new DalOrderItem();
