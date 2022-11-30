a8e59a85616d70406402e24a7b04de98c9133f45
Aharon Katz <ahkatz@g.jct.ac.il>
2022-11-30 12:52:08 +0200

    blorder change

--- BL/BlApi/ICart.cs
+++ BL/BlApi/ICart.cs
@@ -4,6 +4,6 @@ namespace BlApi;
 public interface ICart
 {
     public Cart AddPToCart(Cart cart, int productId);
-    public Cart UpdateAmount(Cart cart, int productId, int amount);
-    public void CreateOrder(Cart cart);
+    public Cart UpdateAmount(Cart cart, int productId, int Namount);
+    public void ConfirmationOrder(Cart cart);
 }
