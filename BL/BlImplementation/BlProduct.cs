6bb97ec6498e6fad12419edbb2771098cc2fd064
Aharon Katz <ahkatz@g.jct.ac.il>
2022-12-01 12:16:43 +0200

    Blproduct changes

--- BL/BlImplementation/BlProduct.cs
+++ BL/BlImplementation/BlProduct.cs
@@ -73,14 +73,71 @@ internal class BlProduct : IProduct
 
     public void AddProductAdmin(BO.Product product)
     {
-        if(product.ID <= 0 || product.Name == null || product.Price <= 0 || product.InStock < 0)
+        if(product.ID < 1000000 || product.ID > 999999 || product.Name == null || product.Price <= 0 || product.InStock < 0)
             throw new BO.InvalidInputException();
-        DO.Product dProduct = new DO.Product();
+
+        DO.Product dProduct = new DO.Product()
+        {
+            ID = product.ID,
+            Name = product.Name,
+            Price = product.Price,
+            InStock = product.InStock,
+            Category = (DO.Category)product.Category
+        };
+     
+        try
+        {
+            Dal.Product.Add(dProduct);
+        }
+        catch (Exception e)
+        {
+            throw new BO.NotExsitsException(e);
+        }
     }
 
     public void DeleteProductAdmin(int productId)
     {
-        BO.Product product = new BO.Product();
-        product = 
+        List<DO.OrderItem> orderItems= new List<DO.OrderItem>();
+        orderItems = (List<DO.OrderItem>)Dal.OrderItem.GetAll();
+
+        foreach (var item in orderItems)
+        {
+            if(item.ProductID == productId)
+                throw new Exception();
+        }
+
+        try
+        {
+            Dal.Product.Delete(productId);
+        }
+        catch (Exception e)
+        {
+
+            throw new BO.NotExsitsException(e);
+        }
+    }
+
+    public void UpdateProductAdmin(BO.Product product)
+    {
+        if (product.ID < 1000000 || product.ID > 999999 || product.Name == null || product.Price <= 0 || product.InStock < 0)
+            throw new BO.InvalidInputException();
+
+        DO.Product dProduct = new DO.Product()
+        {
+            ID = product.ID,
+            Name = product.Name,
+            Price = product.Price,
+            InStock = product.InStock,
+            Category = (DO.Category)product.Category
+        };
+
+        try
+        {
+            Dal.Product.Update(dProduct);
+        }
+        catch (Exception e)
+        {
+            throw new BO.NotExsitsException(e);
+        }
     }
 }
