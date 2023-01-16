17c756aa9981638550d61547ed2788f50f72f23b
Aharon Katz <ahkatz@g.jct.ac.il>
2023-01-16 19:19:46 +0200

    xml files

---
+++ DalXml/DalOrder.cs
@@ -0,0 +1,34 @@
+﻿namespace Dal;
+using DalApi;
+using DO;
+using System.Security.Principal;
+
+internal class DalOrder : IOrder
+{
+    const string s_Order = $"Order";
+
+    #region Add Order
+    /// <summary>
+    /// add a order to xml file
+    /// </summary>
+    ///
+
+    public int Add(Order order)
+    {
+        List<Order?> orders = XMLTools.LoadListFromXMLSerializer<Order>(s_Order);
+
+        if (orders.FirstOrDefault(it => it?.ID == order.ID) != null)
+            throw new Exception("order olready exist");
+
+        orders.Add(order);
+
+        XMLTools.SaveListToXMLSerializer(orders, s_Order);
+
+        return order.ID;
+    }
+
+
+
+
+
+}
---
+++ DalXml/XMLTools.cs
@@ -0,0 +1,60 @@
+﻿using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace Dal;
+
+static class XMLTools
+{
+    const string s_dir = @"..\xml\";
+
+    static XMLTools()
+    {
+        if (!Directory.Exists(s_dir))
+            Directory.CreateDirectory(s_dir);
+    }
+
+    public static void SaveListToXMLSerializer<T>(List<T?> list, string entity)
+    {
+        string filePath = $"{s_dir + entity}.xml";
+
+        try
+        {
+            using FileStream file = new(filePath, FileMode.Create, FileAccess.Write, FileShare.None);
+
+            XmlSerializer serializer = new XmlSerializer(typeof(List<T?>));
+
+            serializer.Serialize(file, list);
+        }
+        catch (Exception e)
+        {
+
+            throw new Exception($"fail to create xml file: {filePath}", e);
+        }
+    }
+    
+    public static List<T?> LoadListFromXMLSerializer<T>(string entity) where T: struct
+    {
+        string filePath = $"{s_dir + entity}.xml";
+
+        try
+        {
+            if (!File.Exists(filePath)) return new();
+
+            using FileStream file = new(filePath, FileMode.Open);
+
+            XmlSerializer x = new(typeof(List<T?>));
+            return x.Deserialize(file) as List<T?> ?? new();
+        }
+        catch (Exception e)
+        {
+
+            throw new Exception($"fail to load xml file:{filePath}", e);
+        }
+    }
+
+
+    
+
+
+}
