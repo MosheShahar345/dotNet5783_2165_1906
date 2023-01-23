e29797f92d1f4e2c8c61ae9843139c82c42afe19
Aharon Katz <ahkatz@g.jct.ac.il>
2023-01-23 18:43:57 +0200

    add comments in dal

--- DalFacade/DO/OrderItem.cs
+++ DalFacade/DO/OrderItem.cs
@@ -7,15 +7,38 @@
 namespace DO;
 
 /// <summary>
-///  geter's and seter's for all variables in order item
+/// This struct represents an OrderItem
 /// </summary>
 public struct OrderItem
 {
+    /// <summary>
+    /// ID of the OrderItem
+    /// </summary>
     public int ID { get; set; }
+
+    /// <summary>
+    /// ID of the Product
+    /// </summary>
     public int ProductID { get; set; }
-    public int OrderID { get; set; } 
+
+    /// <summary>
+    /// ID of the Order
+    /// </summary>
+    public int OrderID { get; set; }
+
+    /// <summary>
+    /// Price of the OrderItem
+    /// </summary>
     public double Price { get; set; }
+
+    /// <summary>
+    /// Amount of the product
+    /// </summary>
     public int Amount { get; set; }
+
+    /// <summary>
+    /// Overridden method that returns a string representation of the OrderItem
+    /// </summary>
     public override string ToString() => $@"
         Order item ID: {ID}
         Product ID: {ProductID}
--- DalFacade/DO/Product.cs
+++ DalFacade/DO/Product.cs
@@ -11,16 +11,34 @@ namespace DO;
 /// </summary>
 public struct Product
 {
+    /// <summary>
+    /// The unique identifier for the product.
+    /// </summary>
     public int ID { get; set; }
+    /// <summary>
+    /// The name of the product.
+    /// </summary>
     public string? Name { get; set; }
+    /// <summary>
+    /// The price of the product.
+    /// </summary>
     public double Price { get; set; }
+    /// <summary>
+    /// The category of the product.
+    /// </summary>
     public Category? Category { get; set; }
+    /// <summary>
+    /// The number of items in stock.
+    /// </summary>
     public int InStock { get; set; }
+    /// <summary>
+    /// A string representation of the product.
+    /// </summary>
     public override string ToString() => $@"
         Product ID: {ID}
         Name: {Name}
         Category: {Category}
-    	Price: {Price}
-    	Amount in stock: {InStock}
+        Price: {Price}
+        Amount in stock: {InStock}
 ";
 }
